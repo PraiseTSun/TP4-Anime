90d587409e3b42e5f8ee38fc62daba810f2987da
Samir Badi <samirbadi1@outlook.com>
2021-11-29 09:02:17 -0500

    Add final variable for each chooses.

--- src/main/java/org/example/AnimeHandler.java
+++ src/main/java/org/example/AnimeHandler.java
@@ -1,5 +1,27 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AnimeHandler {
+    private final String PROGRAMME_TITLE = "****************************************************************\n" +
+                                           "*                  ANIME API QUERY PRO V1.4                    *\n" +
+                                           "****************************************************************";
+    private final String LINE =            "----------------------------------------------------------------";
+    private final String REQUETE_KEY = "a";
+    private final String TYPE_KEY = "b";
+    private final String RATED_KEY = "c";
+    private final String RECHERCHE_KEY = "r";
+    private final String QUITTER_KEY = "q";
+    private final List<String> CHOOSE_LIST = new ArrayList<>(List.of(
+            RECHERCHE_KEY,
+            TYPE_KEY,
+            RATED_KEY,
+            RECHERCHE_KEY,
+            QUITTER_KEY
+    ));
+
+
+
 
 }
