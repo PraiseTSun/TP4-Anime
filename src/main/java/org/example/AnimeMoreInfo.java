f5826f819c570cbf798b009269fa22bc78a11990
Samir_Badi <samirbadi1@outlook.com>
2021-11-30 08:54:31 -0500

    Create the class Date

--- src/main/java/org/example/AnimeMoreInfo.java
+++ src/main/java/org/example/AnimeMoreInfo.java
@@ -1,5 +1,20 @@
 package org.example;
 
 public class AnimeMoreInfo {
+    private String request_hash;
+    private boolean request_cached;
+    private int request_cache_expiry;
+    private int mal_id;
+    private String url;
+    private String image_url;
+    private String trailer_url;
+    private String title;
+    private String title_english;
+    private String title_japanese;
+    private String title_synonyms;
+    private String type;
+    private String source;
+    private int episodes;
+    private String status;
 
 }
